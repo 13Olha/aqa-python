a2d192e8440bf2071a6f120a85e27ecc50f4ab3d
13Olha <olgha.gavryliv.92@gmail.com>
2023-02-02 12:09:17 +0300

    Pytest crud requests and logging homework refactoring

--- test_hw_crud.py
+++ test_hw_crud.py
@@ -4,7 +4,7 @@ from assertpy import assert_that
 import pytest
 
 
-URL = "https://reqres.in"
+BASE_URL = "https://reqres.in"
 
 
 # logger setup
@@ -25,38 +25,46 @@ def change_data():
     return {}
 
 
-def test_get(change_data):
-    response = requests.get(f"{URL}/api/users?page=2")
+@pytest.fixture
+def headers():
+    return {"Content-Type": "application/json; charset=UTF-8'"}
+
+
+@pytest.fixture
+def upsert_data():
+    return {"name": "morpheus", "job": "leader"}
+
+
+def test_get(headers):
+    response = requests.get(f"{BASE_URL}/api/users?page=2", headers=headers)
     response_data = response.json()
     assert response.status_code == 200
     assert response_data["page"] == 2
     assert_that(response_data).contains_key("total_pages")
 
 
-def test_get_users(change_data):
-    response = requests.get(f"{URL}/api/users/2")
+def test_get_users(headers):
+    response = requests.get(f"{BASE_URL}/api/users/2", headers=headers)
     response_data = response.json()
     assert response.status_code == 200
     assert_that(response_data).contains_key("data")
     assert response_data["data"]["id"] == 2
 
 
-def test_post_users(change_data):
-    post_data = '{"name": "morpheus", "job": "leader"}'
-    response = requests.post(f"{URL}/api/users", post_data)
+def test_post_users(headers, upsert_data):
+    response = requests.post(f"{BASE_URL}/api/users", json=upsert_data, headers=headers)
     response_data = response.json()
     assert response.status_code == 201
     assert_that(response_data).contains_key("createdAt")
 
 
-def test_put_users():
-    put_data = '{"name": "morpheus", "job": "leader"}'
-    response = requests.put(f"{URL}/api/users/2", put_data)
+def test_put_users(headers, upsert_data):
+    response = requests.put(f"{BASE_URL}/api/users/2", json=upsert_data, headers=headers)
     response_data = response.json()
     assert response.status_code == 200
     assert_that(response_data).contains_key("updatedAt")
 
 
-def test_delete_user():
-    response = requests.delete(f"{URL}/api/users/2")
+def test_delete_user(headers):
+    response = requests.delete(f"{BASE_URL}/api/users/2", headers=headers)
     assert response.status_code == 204
