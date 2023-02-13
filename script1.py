4cd0b644f164b3ffc40327fc1289984a1516aa4e
13Olha <olgha.gavryliv.92@gmail.com>
2023-02-13 17:51:11 +0300

    multy examples update

--- script1.py
+++ script1.py
@@ -1,9 +1,22 @@
-# def count_words(plates_list):
-#
-#      plates_list = input().split()
-#
-#      plates_list = 'words.txt'
-#      count_words(plates_list)
+class A:
 
- plates_list = input('Enter your name:')
-     print('Hello, ' + x)
+    def __init__(self, value):
+        self.value_a = 20
+    a_property = 1
+
+
+class B:
+    b_property = 2
+
+
+class C (A, B):
+    def __init__(self, value):
+        super().__init__(value + 10)
+        self.value = value
+    c_property = 3
+
+
+inst = C(10)
+
+
+print(inst.a_property, inst.c_property, inst.b_property, inst.value, inst.value_a)
